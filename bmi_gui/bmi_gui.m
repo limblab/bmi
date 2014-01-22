315569556946283eac47864a7c29ab42663e324f
Christian Ethier <christian.ethier@gmail.com>
2014-01-22 18:23:19 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@5 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- bmi_gui/bmi_gui.m
+++ bmi_gui/bmi_gui.m
@@ -22,7 +22,7 @@ function varargout = bmi_gui(varargin)
 
 % Edit the above text to modify the response to help bmi_gui
 
-% Last Modified by GUIDE v2.5 13-Jan-2014 18:12:45
+% Last Modified by GUIDE v2.5 20-Jan-2014 14:17:00
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -61,7 +61,6 @@ guidata(hObject, handles);
 % UIWAIT makes bmi_gui wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
-
 % --- Outputs from this function are returned to the command line.
 function varargout = bmi_gui_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
@@ -72,32 +71,11 @@ function varargout = bmi_gui_OutputFcn(hObject, eventdata, handles)
 % Get default command line output from handles structure
 varargout{1} = handles.output;
 
-
-% --- Executes on button press in cerebus_radio.
-function cerebus_radio_Callback(hObject, eventdata, handles)
-% hObject    handle to cerebus_radio (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% Hint: get(hObject,'Value') returns toggle state of cerebus_radio
-    set(handles.input_file_txtbox,'enable','off');
-    set(handles.input_file_browse_button,'enable','off');
-
-% --- Executes on button press in off_file_radio.
-function off_file_radio_Callback(hObject, eventdata, handles)
-% hObject    handle to off_file_radio (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-% Hint: get(hObject,'Value') returns toggle state of off_file_radio
-    set(handles.input_file_txtbox,'enable','on');
-    set(handles.input_file_browse_button,'enable','on');
-
-% --- Executes on button press in input_file_browse_button.
+%--- Executes on button press in input_file_browse_button.
 function input_file_browse_button_Callback(hObject, eventdata, handles)
 % hObject    handle to input_file_browse_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
-
 
 function input_file_txtbox_Callback(hObject, eventdata, handles)
 % hObject    handle to input_file_txtbox (see GCBO)
@@ -106,7 +84,6 @@ function input_file_txtbox_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of input_file_txtbox as text
 %        str2double(get(hObject,'String')) returns contents of input_file_txtbox as a double
-
 
 % --- Executes during object creation, after setting all properties.
 function input_file_txtbox_CreateFcn(hObject, eventdata, handles)
@@ -120,7 +97,25 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
+% --- Executes when selected object is changed in inputs_panel.
+function inputs_panel_SelectionChangeFcn(hObject, eventdata, handles)
+% hObject    handle to the selected object in inputs_panel 
+% eventdata  structure with the following fields (see UIBUTTONGROUP)
+%	EventName: string 'SelectionChanged' (read only)
+%	OldValue: handle of the previously selected object or empty if none was selected
+%	NewValue: handle of the currently selected object
+% handles    structure with handles and user data (see GUIDATA)
+h = get(handles.inputs_panel,'SelectedObject');
+s = get(h,'String');
+if strcmp(s,'Cerebus')
+    set(handles.input_file_txtbox,'enable','off');
+    set(handles.input_file_browse_button,'enable','off');
+else
+    set(handles.input_file_txtbox,'enable','on');
+    set(handles.input_file_browse_button,'enable','on');
+end
 
+%% Outputs panel
 
 function output_file_txtbox_Callback(hObject, eventdata, handles)
 % hObject    handle to output_file_txtbox (see GCBO)
@@ -156,37 +151,14 @@ function save_output_cbx_Callback(hObject, eventdata, handles)
 % hObject    handle to save_output_cbx (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Hint: get(hObject,'Value') returns toggle state of save_output_cbx
-
-
-% --- Executes on button press in xpc_radio.
-function xpc_radio_Callback(hObject, eventdata, handles)
-% hObject    handle to xpc_radio (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of xpc_radio
-
-
-% --- Executes on button press in stimulator_radio.
-function stimulator_radio_Callback(hObject, eventdata, handles)
-% hObject    handle to stimulator_radio (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of stimulator_radio
-
-
-% --- Executes on button press in no_outputs_radio.
-function no_outputs_radio_Callback(hObject, eventdata, handles)
-% hObject    handle to no_outputs_radio (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Hint: get(hObject,'Value') returns toggle state of no_outputs_radio
-
-
+if get(hObject,'Value')
+    set(handles.output_file_txtbox, 'enable','on');
+    set(handles.output_file_browse_button,'enable','on');
+else
+    set(handles.output_file_txtbox, 'enable','off');
+    set(handles.output_file_browse_button,'enable','off');
+end
 
 function emg_decoder_txtbox_Callback(hObject, eventdata, handles)
 % hObject    handle to emg_decoder_txtbox (see GCBO)
@@ -269,36 +241,102 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-
-% --- Executes on button press in pushbutton4.
-function pushbutton4_Callback(hObject, eventdata, handles)
-% hObject    handle to pushbutton4 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-
 % --- Executes on button press in adapt_enable_cbx.
 function adapt_enable_cbx_Callback(hObject, eventdata, handles)
 % hObject    handle to adapt_enable_cbx (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
 % Hint: get(hObject,'Value') returns toggle state of adapt_enable_cbx
+if get(hObject,'Value')
+    set(handles.adapt_params_textbox_label,'enable','on');
+    set(handles.adapt_params_txtbox, 'enable','on');
+    set(handles.adapt_params_browse_button,'enable','on');
+    set(handles.adapt_lr_label,'enable','on');
+    set(handles.adapt_delay_label,'enable','on');
+    set(handles.adapt_duration_label,'enable','on');
+    set(handles.adapt_lr_txtbox,'enable','on');
+    set(handles.adapt_delay_txtbox,'enable','on');
+    set(handles.adapt_duration_txtbox,'enable','on');
+else
+    set(handles.adapt_params_textbox_label,'enable','off');
+    set(handles.adapt_params_txtbox, 'enable','off');
+    set(handles.adapt_params_browse_button,'enable','off');
+    set(handles.adapt_lr_label,'enable','off');
+    set(handles.adapt_delay_label,'enable','off');
+    set(handles.adapt_duration_label,'enable','off');
+    set(handles.adapt_lr_txtbox,'enable','off');
+    set(handles.adapt_delay_txtbox,'enable','off');
+    set(handles.adapt_duration_txtbox,'enable','off');    
+end
 
-
-% --- Executes on button press in emg_cascade_button.
-function emg_cascade_button_Callback(hObject, eventdata, handles)
-% hObject    handle to emg_cascade_button (see GCBO)
+% --- Executes on button press in adapt_params_browse_button.
+function adapt_params_browse_button_Callback(hObject, eventdata, handles)
+% hObject    handle to adapt_params_browse_button (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hint: get(hObject,'Value') returns toggle state of emg_cascade_button
 
 
-% --- Executes on button press in direct_mode_button.
-function direct_mode_button_Callback(hObject, eventdata, handles)
-% hObject    handle to direct_mode_button (see GCBO)
+function adapt_lr_txtbox_Callback(hObject, eventdata, handles)
+% hObject    handle to adapt_lr_txtbox (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-% Hint: get(hObject,'Value') returns toggle state of direct_mode_button
+% Hints: get(hObject,'String') returns contents of adapt_lr_txtbox as text
+%        str2double(get(hObject,'String')) returns contents of adapt_lr_txtbox as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function adapt_lr_txtbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to adapt_lr_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function adapt_delay_txtbox_Callback(hObject, eventdata, handles)
+% hObject    handle to adapt_delay_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of adapt_delay_txtbox as text
+%        str2double(get(hObject,'String')) returns contents of adapt_delay_txtbox as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function adapt_delay_txtbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to adapt_delay_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+function adapt_duration_txtbox_Callback(hObject, eventdata, handles)
+% hObject    handle to adapt_duration_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of adapt_duration_txtbox as text
+%        str2double(get(hObject,'String')) returns contents of adapt_duration_txtbox as a double
+
+% --- Executes during object creation, after setting all properties.
+function adapt_duration_txtbox_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to adapt_duration_txtbox (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
