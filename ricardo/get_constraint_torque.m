55f81559ad8bda91d0a0d4e21caf0f9d788c0447
Ricardo Torres <ricardort@gmail.com>
2014-09-08 20:41:15 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@105 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- ricardo/get_constraint_torque.m
+++
@@ -1,4 +0,0 @@
-function constraint_torque = get_constraint_torque(arm_params,theta)
-
-constraint_angle_diff = [theta(1)-arm_params.null_angles(1);theta(2)-(theta(1)+diff(arm_params.null_angles))];
-constraint_torque = -sign(constraint_angle_diff).*exp(40*(abs(constraint_angle_diff))/(pi/2)-25);
