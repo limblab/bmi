de718c5777e9eb10c93eb602c79a961a27fbc47a
Christian Ethier <christian.ethier@gmail.com>
2015-01-21 21:37:43 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@138 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- backpropagation/stim_params_defaults.m
+++ backpropagation/stim_params_defaults.m
@@ -1,31 +1,7 @@
 function stim_params = stim_params_defaults(varargin)
-%   'mode'          : either 'direct' or 'emg_cascade'
-%   'output'        : either 'stimulator' or 'xpc' or 'none'
-%   'adapt'         : enable adaptation (emg_cascade mode only for now)
-%   'LR'            : Learning Rate
-%   'N2E_dec'       : Initial Neuron-to-EMG decoder: 'new_zeros',
-%                     'new_rand', or string with decoder file name.
-%   'n_lag'         : Number of lags to use (Default: 10)
-%   'n_neurons'     : Number of neurons
-%   'neuronIDs'     : Array of n_neurons x 2, containing [ch_id, unit_id];
-%   'display_plots' : Plot adaptation procedure (Default: true)
-%   'n_adapt_to_last':Adapt to last n sets of targets (look back for the
-%                     last n instances in which the target appear and run
-%                     gradient descent on them)
-%   'simulate'      : Simulate four neurons each tuned to different halfs
-%                     of the screen (vertical and horizontal)
-%   'online'        : chose between Cerebus stream or BinnedData file
-%   'realtime'      : # of times x real time (only for offline files)
-%   'adaptation_time':duration of adaptation periods in seconds (default: Inf)
-%   'fixed_time'    : duration of fixed periods in seconds. (def:0)
-%   'adaptation_progress':display adaptation progress (Default: false)
-%   'cursor_assist' : moves the cursor to a target and back, based on average firing rate and 'cursor_traj'
-%   'cursor_traj'   : file path and name to a structure containing the fields 'mean_paths' and 'back_paths'
-%   'delay'         : time between target onset and beginning of adaptation
-%   'duration'      : time window during which to adapt after delay
-%   'db_size'       : expected size of databurst
-%   'save_dir'      : path in which to save updated decoder
-%   'offline_data'  : binnedData file to be replayed (when 'online'=false)
+%
+%
+%
 
 stim_params_defaults = struct( ...   
     'EMG_min', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],...
@@ -33,11 +9,12 @@ stim_params_defaults = struct( ...
     'freq', 30,...
     'mode',1,...
     'PW_max',[0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2 0.2],...
+    'PW_min', [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],...
     'current_min',[0,0,0,0,0,0,0,0,0,0,0,0],...
-    'current_max', [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],...
+    'current_max', [0, 0, 1, 4, 4, 0, 0, 2, 2, 1, 1, 0],...
     'duration_command',[6,6,6,6,6,6,6,6,6,6,6,6],...
-    'anode_map', {{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23; 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1;}},...
-    'cathode_map',  {{2 4 6 8 10 12 14 16 18 20 22 24; 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1;}}...
+    'anode_map', {{ [31], [31], [1], [3, 5, 7, 9], [11, 13, 15, 17],[31],[31], [19, 21], [23, 25], [27], [29],[31]; 1, 1, 1, [.25,.25,.25,.25], [.25,.25,.25,.25], 1, 1, [.5,.5], [.5,.5], 1, 1, 1;}},...
+    'cathode_map',  {{ [31],[31],[2], [4 6 8 10], [12 14 16 18], [31],[31], [20 22], [24 26], [28], [30], [31]; 1, 1, 1, [.25,.25,.25,.25], [.25,.25,.25,.25], 1, 1, [.5,.5], [.5,.5], 1, 1, 1;}}...
  );
 
 % fill default options missing from input argument
@@ -49,7 +26,7 @@ end
 
 all_param_names = fieldnames(stim_params_defaults);
 for i=1:numel(all_param_names)
-    if ~isfield(bmi_params,all_param_names(i))
+    if ~isfield(stim_params,all_param_names(i))
         stim_params.(all_param_names{i}) = stim_params_defaults.(all_param_names{i});
     end
 end
