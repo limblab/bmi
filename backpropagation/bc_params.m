c4cdaf14a5248daf6529d8909b187f5a8d1a9386
Christian Ethier <christian.ethier@gmail.com>
2015-01-22 22:48:27 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@139 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- backpropagation/bc_params.m
+++ backpropagation/bc_params.m
@@ -15,28 +15,33 @@ for i = 1:nargin
     end
 end
 
-%Jaco:
-    params.save_name      = 'Jaco_WF_BC_';
-    params.save_dir       = 'F:\Data-lab1\8I1-Jaco\CerebusData\Adaptation';
-
-    %Neuron Decoder
-    params.neuron_decoder = 'F:\Data-lab1\8I1-Jaco\SavedFilters\2014_07_21\Adapted_decoder_2014_07_21_180540_End.mat';
-    params.n_neurons      = size(params.neuronIDs,1);
-
-    %EMG Decoder
-    params.emg_decoder    = 'F:\Data-lab1\8I1-Jaco\SavedFilters\Jaco_WF_20140720_HC_001_E2F_Decoder.mat';
-    params.n_emgs         = 6;
+% %Jaco:
+%     params.save_name      = 'Jaco_WF_BC_';
+%     params.save_dir       = 'F:\Data-lab1\8I1-Jaco\CerebusData\Adaptation';
+% 
+%     %Neuron Decoder
+%     params.neuron_decoder = 'F:\Data-lab1\8I1-Jaco\SavedFilters\2014_07_21\Adapted_decoder_2014_07_21_180540_End.mat';
+%     params.n_neurons      = size(params.neuronIDs,1);
+% 
+%     %EMG Decoder
+%     params.emg_decoder    = 'F:\Data-lab1\8I1-Jaco\SavedFilters\Jaco_WF_20140720_HC_001_E2F_Decoder.mat';
+%     params.n_emgs         = 6;
     
-% 
-% %Jango:
-%     params.save_name      = 'Jango_WF_BC_';
-%     params.save_dir       = 'F:\Data-lab1\12A1-Jango\CerebusData\Adaptation';
-% 
-%     % Neuron decoder
-% %     params.neuron_decoder = 'new_zeros';
-%     params.neuron_decoder = 'F:\Data-lab1\12A1-Jango\SavedFilters\Jango_WF_20140715_HC_001_N2E_Decoder.mat';
-%     params.n_neurons = size(params.neuronIDs,1);
-% 
-%     % EMG decoder
-%     params.emg_decoder = 'F:\Data-lab1\12A1-Jango\SavedFilters\Jango_WF_20140715_HC_001_E2F_Decoder.mat';
-%     params.n_emgs      = 10;
+
+%Jango:
+    params.save_name      = 'Jango_IsoBox_BC_';
+    params.save_dir       = 'F:\Data-lab1\12A1-Jango\CerebusData\Adaptation\20150120\';
+    params.mode           = 'direct';
+
+    % Neuron decoder
+%     params.neuron_decoder = 'new_zeros';
+%     params.neuron_decoder = 'Z:\Jango_12a1\SavedFilters\BCcontrol\20150115\Jango_IsoBox_HC_20150115_001_N2F_Decoder.mat';
+    params.neuron_decoder = 'Z:\Jango_12a1\SavedFilters\Adaptation\20150120\Jango_20150120_WFHC_003_N2E.mat';
+    params.neuron_decoder = 'Z:\Jango_12a1\SavedFilters\Adaptation\20150120\Jango_20150120_WFHC_003_N2F.mat';
+    params.n_neurons = size(params.neuronIDs,1);
+
+    % EMG decoder
+%     params.emg_decoder = 'Z:\Jango_12a1\SavedFilters\Jango_20150107_E2F_xcor_12EMGs.mat';
+    params.emg_decoder = 'Z:\Jango_12a1\SavedFilters\E2F_8generic_iso_muslces.mat';
+    params.n_emgs      = 8;
+    params.n_lag_emg   = 1;
