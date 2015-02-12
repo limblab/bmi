8ee0be77c6a14815fd06735c90be69fbfbd25933
Christian Ethier <christian.ethier@gmail.com>
2015-02-12 20:41:20 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@148 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- backpropagation/offline_adapt_vs_optimal_decoder_experiment.m
+++ backpropagation/offline_adapt_vs_optimal_decoder_experiment.m
@@ -24,14 +24,9 @@
 %       >> save('Z:\Jango_12a1\SavedFilters\Adaptation\20150212\Jango_20150212_WFHC_001_N2E_adapt_Decoder.mat';
 %
 % 5a - Brain-control using optimal decoder:
-%       >> edit bc_params:
+%       >> edit bc_params: mode and neuron decoder
+%       >> run_decoder(bc_params); % 5 min
 %
-%
-%
-%
-%
-%
-%
-%
-%
-%
+% 5b - Brain-control using non-supervised decoder:
+%       >> edit bc_params: mode and neuron decoder
+%       >> run_decoder(bc_params); % 5 min
