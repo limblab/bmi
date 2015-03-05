09b94e9b15b08625031dbd883bc4e7bcdd6eadae
Christian Ethier <christian.ethier@gmail.com>
2015-03-05 22:50:55 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@161 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

---
+++ default_parameters/curs_traj_default.m
@@ -0,0 +1,9 @@
+function traj = curs_traj_default
+
+%find path of the default trajectory file
+% file is 'default_cursor_traj_R10.mat'
+% it is in the /bmi/lib folder
+
+filename = 'default_cursor_traj_R10.mat';
+traj = load('default_cursor_traj_R10.mat');
+
