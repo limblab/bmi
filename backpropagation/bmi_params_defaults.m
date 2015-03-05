7d099c8908b29951d0e059afbcace8f504f7bbd8
Christian Ethier <christian.ethier@gmail.com>
2015-03-05 22:56:49 +0000

    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@164 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- backpropagation/bmi_params_defaults.m
+++ backpropagation/bmi_params_defaults.m
@@ -1,21 +1,38 @@
 function bmi_params = bmi_params_defaults(varargin)
 %   'mode'          : either 'direct' or 'emg_cascade'
-%   'output'        : either 'stimulator' or 'xpc' or 'none'
 %   'adapt'         : enable adaptation (emg_cascade mode only for now)
-%   'N2E_dec'       : Initial Neuron-to-EMG decoder: 'new_zeros',
+%   'cursor_assist' : moves the cursor to a target and back according to 'cursor_traj'
+%   'cursor_traj'   : file path and name to a structure containing the fields 'mean_paths' and 'back_paths'
+%   'neuron_decoder': Initial Neuron-to-EMG decoder: 'new_zeros',
 %                     'new_rand', or string with decoder file name.
-%   'n_lag'         : Number of lags to use (Default: 10)
-%   'n_neurons'     : Number of neurons
-%   'neuronIDs'     : Array of n_neurons x 2, containing [ch_id, unit_id];
-%   'display_plots' : Plot adaptation procedure (Default: true)
+%   'emg_decoder'   : file name or structure containing emg-to-force model
+%   'output'        : either 'stimulator' or 'xpc' or 'none'
 %   'online'        : chose between Cerebus stream or BinnedData file
 %   'realtime'      : # of times x real time (only for offline files)
-%   'cursor_traj'   : file path and name to a structure containing the fields 'mean_paths' and 'back_paths'
-%   'delay'         : time between target onset and beginning of adaptation
-%   'duration'      : time window during which to adapt after delay
-%   'db_size'       : expected size of databurst
-%   'save_dir'      : path in which to save updated decoder
 %   'offline_data'  : binnedData file to be replayed (when 'online'=false)
+%   'hp_rc'         : high-pass filter time constant in seconds (0 = no filtering of preds)
+%   'stim_params'   : structure containing emg-to-stim parameters and electrode to muscle mapping
+%
+%   'n_neurons'     : Number of neurons
+%   'neuronIDs'     : Array of n_neurons x 2, containing [ch_id, unit_id];
+%   'n_lag'         : Number of lags to use (Default: 10)%
+%   %   'n_emgs'        : Number of muscles in E2F model
+%   'n_lag_emg'     : Number of lags for E2F model
+%   'n_forces'      : Number of force signals to predict (unused?)
+%   'binsize'       : Cycle time for decoder. Has to match binsize in decoders
+%   'db_size'       : expected size of databurst
+%   'ave_fr'        : scalar representing global average firing rate
+%
+%   'display_plots' : Plot adaptation procedure (Default: true)
+%   'print_out'     : if true, gives warnings when processing a cycle takes longer than binsize
+%   'save_data'     : whether or not to save predictions and other data streamed from cerebus
+%   'save_dir'      : directory for saving data
+%   'save_name'     : prefix for saving files names
+%    
+%   'force_offset'  :
+%   'stop_trial'    :
+%   'stop_task_if_x_artifacts' : 
+%   'stop_task_if_x_force' :
 
 N2E = 'Jango_20141203_default_N2F_decoder.mat';
 E2F = E2F_default;
@@ -31,8 +48,8 @@ bmi_params_defaults = struct( ...
     'online'        ,false,...
     'realtime'      ,true,...
     'offline_data'  ,'Jango_20141203_default_offline_data.mat',...
-    ...'N2Esig'        ,[0 1 0.5 10],... %[bottom top v50 slope];
-    ...'stim_params'   ,stim_params_default,...
+    'hp_rc'         ,0,...
+    'stim_params'   ,stim_params_default,...
     ......
     'n_neurons'     ,96,...
     'neuronIDs'     ,[(1:96)' zeros(96,1)],...
@@ -50,7 +67,12 @@ bmi_params_defaults = struct( ...
     'print_out'     ,true,...
     'save_data'     ,false,...
     'save_dir'      ,cd,...
-    'save_name'     ,'BMI_test'...
+    'save_name'     ,'BMI_test',...
+    ...
+    'force_offset'  ,[0 0],...
+    'stop_trial'    ,0, ...
+    'stop_task_if_x_artifacts', 10, ...
+    'stop_task_if_x_force',     0.02 ...
 );
 
 
