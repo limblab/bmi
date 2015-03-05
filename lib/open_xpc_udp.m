80c36f170a9458b14f4f0255c59d0662fbef6e23
Christian Ethier <christian.ethier@gmail.com>
2015-03-05 22:45:19 +0000

    removed the .write from xpc object
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@157 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- lib/open_xpc_udp.m
+++ lib/open_xpc_udp.m
@@ -2,13 +2,20 @@ function xpc = open_xpc_udp(params)
 %% UDP port for XPC
 if strcmpi(params.output,'xpc')
     XPC_IP   = '192.168.0.1';
-    XPC_PORT_WRITE = 24999;
-    echoudp('on',XPC_PORT_WRITE);
-    xpc.xpc_write = udp(XPC_IP,XPC_PORT_WRITE);
-    set(xpc.xpc_write,'ByteOrder','littleEndian');
-    set(xpc.xpc_write,'LocalHost','192.168.0.10');
-    fopen(xpc.xpc_write);
+    XPC_PORT = 24999;
+    echoudp('on',XPC_PORT);
+    xpc = udp(XPC_IP,XPC_PORT);
+    set(xpc,'ByteOrder','littleEndian');
+    set(xpc,'LocalHost','192.168.0.10');
+    fopen(xpc);
 
+%     XPC_PORT_WRITE = 24999;
+%     echoudp('on',XPC_PORT_WRITE);
+%     xpc.xpc_write = udp(XPC_IP,XPC_PORT_WRITE);
+%     set(xpc.xpc_write,'ByteOrder','littleEndian');
+%     set(xpc.xpc_write,'LocalHost','192.168.0.10');
+%     fopen(xpc.xpc_write);
+% 
 %     XPC_PORT_READ = 24998;
 %     xpc.xpc_read = udp(XPC_IP,XPC_PORT_READ);
 %     set(xpc.xpc_read,'ByteOrder','littleEndian');
@@ -18,4 +25,6 @@ if strcmpi(params.output,'xpc')
 %     set(xpc.xpc_read,'InputBufferSize',100);
 %     set(xpc.xpc_read,'InputDatagramPacketSize',100);
 %     set(xpc.xpc_read, 'ReadAsyncMode', 'continuous');
+else
+    xpc = [];
 end
