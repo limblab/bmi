f2779c4865056aa9358d3715aea04d42c44f1d3e
Christian Ethier <christian.ethier@gmail.com>
2015-03-25 20:12:10 +0000

    in get_new_spikes, now making sure we are matching the appropriate data channels with the decoder channels
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@179 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- fes_bmi/run_decoder2.m
+++ fes_bmi/run_decoder2.m
@@ -437,8 +437,8 @@ function new_spikes = get_new_spikes(ts_cell_array,params,binsize)
  
     %firing rate for new spikes
     for i = 1:params.n_neurons
-        unit = params.neuronIDs(i,2);
-        new_spikes(i) = length(new_ts{i,unit+2})/binsize;
+        [chan,unit] = params.neuronIDs(i,:);
+        new_spikes(i) = length(new_ts{chan,unit+2})/binsize;
     end
 
 %     %remove artifact (80% of neurons have spikes for this bin)
