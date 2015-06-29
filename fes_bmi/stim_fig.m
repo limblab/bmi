ba2eaa58ce0af605b5a5255630f9b70646a54141
Juan Gallego <jotagalabe@gmail.com>
2015-06-29 01:28:14 +0000

    Fixed the function. It now plots the PW in real-time
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@221 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- fes_bmi/stim_fig.m
+++ fes_bmi/stim_fig.m
@@ -8,13 +8,29 @@ if strcmp(bmi_fes_stim_params.mode,'PW_modulation')
     figure(fig_handle)
     
     % label the muscles to use different colors
+    ext_muscles             = strncmp(bmi_fes_stim_params.muscles,'E',1);
+    flex_muscles            = strncmp(bmi_fes_stim_params.muscles,'F',1);
     
+    hand_muscles            = strncmp(bmi_fes_stim_params.muscles,'ADL',3);
+    hand_muscles            = hand_muscles | strncmp(bmi_fes_stim_params.muscles,'APB',3);
+    hand_muscles            = hand_muscles | strncmp(bmi_fes_stim_params.muscles,'APL',3);
     
-    bar(stim_PW,'r')
+    other_muscles           = strncmp(bmi_fes_stim_params.muscles,'Brad',4);
+    other_muscles           = other_muscles | strncmp(bmi_fes_stim_params.muscles,'Sup',3);
+    other_muscles           = other_muscles | strncmp(bmi_fes_stim_params.muscles,'PT',2);
+
+    
+    bar(stim_PW)
     set(gca,'XTickLabel',bmi_fes_stim_params.muscles)
+    hold on    
+    bar(find(ext_muscles),stim_PW(ext_muscles),'b')
+    bar(find(flex_muscles),stim_PW(flex_muscles),'r')
+	bar(find(hand_muscles),stim_PW(hand_muscles),'k')
+    bar(find(other_muscles),stim_PW(other_muscles),'g')
     xlim([.5 length(stim_PW)+.5]), ylim([0 max(bmi_fes_stim_params.PW_max)])
     xlabel('muscle')
     ylabel('PW (us)')
+    hold off
 else
     
 end
