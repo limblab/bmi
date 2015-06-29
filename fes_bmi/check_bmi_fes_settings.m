a0459748629b9293f74a6c9d581b7e40fb410c7c
Juan Gallego <jotagalabe@gmail.com>
2015-06-29 01:26:47 +0000

    Function that checks if the stimulation settings in bmi_fes_stim_params have consistent dimensions, and whether this is the same as in the EMG decoder
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@218 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

---
+++ fes_bmi/check_bmi_fes_settings.m
@@ -0,0 +1,34 @@
+% Function that makes a security check on the parameters for
+% intracortically-controlled FES
+
+function check_bmi_fes_settings( neuron_decoder, params )
+
+
+% check that everything in params.bmi_fes_stim_params is consistent
+% size-wise
+
+params_to_check             = [1:3 6:11];
+nbr_muscles_bmi_fes         = zeros(1,length(params_to_check));
+
+all_param_names             = fieldnames(params.bmi_fes_stim_params);
+
+for i = 1:length(params_to_check)
+    nbr_muscles_bmi_fes(i)  = size(params.bmi_fes_stim_params.(all_param_names{params_to_check(i)}),2);
+end
+
+if size(params.bmi_fes_stim_params.cathode_map,2) > 0
+    nbr_muscles_bmi_fes     = [nbr_muscles_bmi_fes, size(params.bmi_fes_stim_params.cathode_map,2)];
+end
+
+if numel(unique(nbr_muscles_bmi_fes)) > 1
+    error('Some of the parameters in bmi_fes_stim_params do not have the right dimension')
+    return;
+end
+
+
+% check that we nbr of decoded EMGs we have specified match the 
+nbr_emgs_decoder            = size(neuron_decoder.H,2);
+
+if nbr_emgs_decoder ~= unique(nbr_muscles_bmi_fes)
+    warning('the number of decoded EMGs does not match bmi_fes_stim_params')
+end
