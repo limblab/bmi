013c711a86dbfdcf95e592e20ceccf071c7de913
Juan Gallego <jotagalabe@gmail.com>
2015-07-10 20:45:44 +0000

    Script to easily configure ICMS for the tDCS experiment
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@258 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

---
+++ fes_stim/ICMS/setup_ICMS_4_tDCS_experiment.m
@@ -0,0 +1,36 @@
+%
+% This script configures the sta_trig_avg for our TDCS experiment
+%
+
+% Who is in the lab?
+monkey                  = 'Kevin';  % 'Jango'
+
+
+% Set up the stimulation parameters
+
+stap                    = stim_trig_avg_defaults;
+
+stap.nbr_stims_ch       = 15000;
+
+
+switch monkey
+    case 'Kevin'
+        stap.monkey     = 'Kevin';
+        stap.data_dir   = 'E:\Data-lab1\12A2-Kevin\CerebusData\TDCS';
+        stap.stim_elec  = 1;
+    case 'Jango'
+        stap.monkey     = 'Jango';
+        stap.data_dir   = 'E:\Data-lab1\12A1-Jango\CerebusData\TDCS';
+        stap.stim_elec  = 17;
+    otherwise
+        error('wrong monkey!');
+end
+        
+
+
+%% -----------------------------------------------------------------------
+% Call the ICMS function
+
+stim_trig_avg(stap);
+
+
