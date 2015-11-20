2e51ee0b9ba4204d768149dfa61a4f295049e9d6
Juan Gallego <jotagalabe@gmail.com>
2015-11-20 22:52:54 +0000

    updated to allow for a sync out signal
    git-svn-id: http://citadel.physiology.northwestern.edu/svn/bmi@330 6bc265fc-7bd1-474a-887e-5d565c7d7ba7

--- fes_stim/GTO_stim.m
+++ fes_stim/GTO_stim.m
@@ -1,19 +1,20 @@
 %
-% function GTO_stim( stim_ampl, stim_pw, stim_freq, nbr_pulses ) 
+% function GTO_stim( stim_ampl, stim_pw, stim_freq, nbr_pulses, mode, sync_out ) 
 %
 %   stim_ampl: in mA ( < 8 mA)
 %   stim_pw: in ms
 %   stim_freq: in Hz
 %   nbr_pulses: (duration has to be < 1 s)
 %   mode: 'bi' or 'mono' (-polar)
+%   sync_out: optional. if true, it will send out a single sync stimulus
+%       through channel 32
 
-function GTO_stim( stim_ampl, stim_pw, stim_freq, nbr_pulses, mode ) 
+function GTO_stim( stim_ampl, stim_pw, stim_freq, nbr_pulses, mode, varargin ) 
 
-sp              = stim_params_defaults();
+sp                      = stim_params_defaults();
 
-sp.tl           = 1000/stim_freq*nbr_pulses;
-sp.amp          = stim_ampl/4*ones(1,8);
-sp.freq         = stim_freq;
+sp.tl                   = 1000/stim_freq*nbr_pulses;
+sp.freq                 = stim_freq;
 
 switch mode
     case 'bi'
@@ -23,6 +24,21 @@ switch mode
         sp.elect_list   = [2 4 6 8];
         sp.pol          = [1 1 1 1];
 end        
-sp.pw           = stim_pw;
+sp.pw                   = stim_pw;
+sp.amp                  = round(stim_ampl/4) ...
+                            * ones(1,numel(sp.elect_list));
+
+
+% If sync_out is set, send out sync signal (single pulse, 2.2 mA) through
+% channel 32 
+if nargin == 6
+    if sync_out
+        sp.tl           = [sp.tl * ones(1,numel(elect_list)), ...
+                                1000/sta_params.stim_freq];
+        sp.amp          = [sp.amp, 127];
+        sp.elect_list   = [sp.elect_list, 32];
+        sp.pol          = [sp.pol, 1];
+    end
+end
 
 ezstim(sp);
