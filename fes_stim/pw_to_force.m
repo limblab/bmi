104bc10781bb6933dc97c0e697beb42e5491527c
juangallego <jotagalabe@gmail.com>
2016-02-23 14:51:48 -0600

    fixed a few bugs. On the works

--- fes_stim/pw_to_force.m
+++ fes_stim/pw_to_force.m
@@ -5,6 +5,9 @@
 %   force = PW_TO_FORCE( force )
 %   force = PW_TO_FORCE( force, pw_to_f_params )
 %
+%   Note: the default params struct needs to be fixed !!!!!
+%   Note2: need to add calculations and plots of raw forces, now only
+%   rectified
 
 function force = pw_to_force( force, varargin )
 
@@ -16,98 +19,80 @@ elseif nargin == 2
     params              = varargin{1};
 end
 
-%------------------------------------
-% Preliminary stuff
 
-% Preallocate a matrix to store evoke force. 
-% Dimensions are: evoked force -by- force sensor -by- stimulus nbr -by- PW
-aux.evoked_resp_length  = ( params.t_before + force.meta.stim_dur + params.t_after) ...
-                            / 1000 * force.fs + 1; % in samples
-aux.nbr_pws             = numel( force.meta.stim_pws );
-force.evoked_force      = zeros( aux.evoked_resp_length, force.nbr_forces, ...
-                            force.meta.nbr_stims, aux.nbr_pws );
+% define struct to store some stuff
+aux                 = struct();
+aux.nbr_pws         = length(force.meta.stim_pws);
 
-                      
-% -------------------------------------------------------------------------
-% Check that the evoked responses fall within the recorded dataset, and
-% fill the evoked responses matrix --à la STA
 
-% calculate how many samples before and after the stimulus we need to look
-% at for characterizing the evoked responses
-aux.win_start           = - params.t_before / 1000 * force.fs;
-aux.win_end             = (force.meta.stim_dur + params.t_after ) / 1000 * force.fs;
-
-% check that neither the time before the first stimulus train, nor the time
-% after the end of the last stimulus train, fall outside the recorded data
-% window
-if force.t_sync_pulses(1)*force.fs < abs(aux.win_start)
-    warning('the first stimulus falls to early within the recorded data window; it will be discarded');
-    force.t_sync_pulses(1) = [];
-end
-if ( force.t_sync_pulses(end)*force.fs + aux.win_end ) > ( force.data(end,1) * force.fs )
-    warning('the last stimulus falls to late within the recorded data window; it will be discarded');
-    force.t_sync_pulses(end) = [];
-end
-
-% Fill the 'evoked_force' matrix
-% do for each PW
-for i = 1:aux.nbr_pws
-    
-    % find which of the randomly ordered stimuli is at this PW
-    aux.pos_stim        = find( force.stim_pw == force.meta.stim_pws(i) );
-    
-    for ii = 1:numel(aux.pos_stim)
-       
-        % find the sample when the current sync pulse occurs
-       aux.sample_curr_sync = round( force.t_sync_pulses(aux.pos_stim(ii)) * force.fs );
-       % and in which sample the window starts and ends
-       aux.curr_win_start   = aux.win_start + aux.sample_curr_sync;
-       aux.curr_win_end     = aux.win_end + aux.sample_curr_sync;
-       
-       % fill the matrix. Note that column 1 of force.data is the time
-       force.evoked_force(:,:,ii,i) = force.data( aux.curr_win_start:aux.curr_win_end, ...
-                                2:force.nbr_forces+1 ); 
-    end
-end
-
-                        
 % -------------------------------------------------------------------------
 % The analysis itself
 
 % 1. compute the mean and SD evoked response for each PW
 % results will be stored in vectors with dimensions: 
 %   time -by- force sensor -by- PW
-force.analysis.mean     = zeros( aux.evoked_resp_length, force.nbr_forces, aux.nbr_pws );
-force.analysis.std      = zeros( aux.evoked_resp_length, force.nbr_forces, aux.nbr_pws );
+force.analysis.mean         = zeros( force.length_evoked_force, force.nbr_forces, aux.nbr_pws );
+force.analysis.std          = zeros( force.length_evoked_force, force.nbr_forces, aux.nbr_pws );
 
 % do for each PW
-for i= 1:aux.nbr_pws 
-    force.analysis.mean(:,:,i) = mean(force.evoked_force(:,:,:,i),3);
-    force.analysis.std(:,:,i) = std(force.evoked_force(:,:,:,i),0,3);
+for i= 1:aux.nbr_pws
+    force.analysis.mean(:,:,i) = mean(force.evoked_force(:,:,force.stim_pws==force.meta.stim_pws(i)),3);
+    force.analysis.std(:,:,i) = std(force.evoked_force(:,:,force.stim_pws==force.meta.stim_pws(i)),0,3);
 %     for ii = 1:force.nbr_forces
 %         force.analysis.mean(:,ii,i) = mean(force.evoked_force(:,ii,:,i),1);
 %     end 
 end
 
-% 2. Compute the peak evoked RECTIFIED force for each PW, as well as its
-% mean and SD 
-% preallocate matrices for data storage
-force.analysis.peak     = zeros( force.meta.nbr_stims, force.nbr_forces, aux.nbr_pws );
-force.analysis.mean_peak    = zeros( force.nbr_forces, aux.nbr_pws );
-force.analysis.std_peak = zeros( force.nbr_forces, aux.nbr_pws );
 
-% calculate peak ABSOLUTE force evoked by each stimulus
-for i = 1:aux.nbr_pws 
+% 2. Compute the peak evoked RECTIFIED force for each PW, as well as its
+% mean and SD
+
+% preallocate matrices for data storage
+force.analysis.peak_rect    = zeros( force.meta.nbr_stims, force.nbr_forces, aux.nbr_pws );
+force.analysis.peak         = zeros( force.meta.nbr_stims, force.nbr_forces, aux.nbr_pws );
+force.analysis.mean_peak_rect = zeros( force.nbr_forces, aux.nbr_pws );
+force.analysis.std_peak_rect  = zeros( force.nbr_forces, aux.nbr_pws );
+
+% get sample at which stimulation started 
+indx_stim                   = params.t_before*force.fs/1000 + 1;
+% get indexes baseline interval 
+int_baseline                = 1:indx_stim-1; 
+
+% calculate peak ABSOLUTE and RAW (signed) force evoked by each stimulus,
+% by simply looking for the maximum of the evoked force in each direction 
+for i = 1:aux.nbr_pws
     for ii = 1:force.nbr_forces
-        force.analysis.peak(:,ii,i) = max(abs(force.evoked_force(:,ii,:,i)));
+        indx_this_pw        = find(force.stim_pws==force.meta.stim_pws(i));
+%        [aux_max, indx_max] = max(abs(force.evoked_force(indx_stim:end,ii,force.stim_pws==force.meta.stim_pws(i))));
+        [aux_max, indx_max] = max(abs(force.evoked_force(indx_stim:end,ii,indx_this_pw)));
+        force.analysis.peak_rect(:,ii,i) = squeeze(aux_max);
+        indx_max            = squeeze(indx_max);
+%         % ToDo
+%         % peak raw (signed) force
+%         for iii = 1:length(indx_max)
+%             force.analysis.peak(:,ii,i) = force.evoked_force(indx_stim+indx_max(iii)-1,ii,indx_this_pw);
+%         end
     end
 end
 
+% calculate baseline rectif force
+% ToDo
+
+
 % calculate mean and SD
-for i = 1:aux.nbr_pws  
-    force.analysis.mean_peak(:,i) = mean(force.analysis.peak(:,:,i),1);
-    force.analysis.std_peak(:,i) = std(force.analysis.peak(:,:,i),0,1);
+for i = 1:aux.nbr_pws
+    for ii = 1:force.nbr_forces
+        force.analysis.mean_peak_rect(ii,i) = mean(force.analysis.peak_rect(:,ii,i),1);
+        force.analysis.std_peak_rect(ii,i) = std(force.analysis.peak_rect(:,ii,i),0,1);
+    end
 end
+
+
+% 2. Compute the 2D projection. This assumes the monkey is in the isometric
+% box
+
+
+
 
 % -------------------------------------------------------------------------
 % Plots
@@ -115,78 +100,91 @@ end
 % handle to maximized figure
 figure('units','normalized','outerposition',[0 0 1 1]);
 % create vector with colors per PW
-aux.colors          = jet(aux.nbr_pws);
+aux.colors                  = jet(aux.nbr_pws);
 
 %------------------------------------
 % 1. evoked responses colored according to PW level
 
 % create vector with time (ms)
-aux.t_evoked_force  = ( aux.win_start:aux.win_end ) * 1000 / force.fs;
+aux.t_evoked_force          = - params.t_before : 1000/force.fs : ...
+                                params.t_after+params.train_dur;
+% PW axis, in us
+aux.pw_axis                 = round(force.meta.stim_pws*1000); % round because sometimes linspace does weird things with the precision
 % vector for storing handles to plots - for color legend
-aux.color_h         = zeros(force.nbr_forces,force.meta.nbr_stims,aux.nbr_pws);
+aux.colors                  = jet(aux.nbr_pws);
+% create string for legend
+for i = 1:aux.nbr_pws
+    lgnd{i} = [num2str(force.meta.stim_pws(i)*1000) ' us'];
+end
+% 
 
 % plot each evoked response
 for i = 1:force.nbr_forces
     subplot(force.nbr_forces,2,1+2*(i-1)), hold on
     for ii = 1:aux.nbr_pws
-        aux.color_h(i,:,ii) = plot(aux.t_evoked_force,squeeze(force.evoked_force(:,i,:,ii)),'color',aux.colors(ii,:));
+        plot(aux.t_evoked_force,squeeze(force.evoked_force(:,i,force.stim_pws==force.meta.stim_pws(ii))),'color',aux.colors(ii,:));
+        % plot mean response
+        axl(ii) = plot(aux.t_evoked_force,force.analysis.mean(:,i,ii),'color',aux.colors(ii,:),'linewidth',3,'linestyle','-.');
     end
     xlim([aux.t_evoked_force(1) aux.t_evoked_force(end)])
-    set(gca,'FontSize',16)
-end
-
-% and the mean...
-for i = 1:force.nbr_forces
-    subplot(force.nbr_forces,2,1+2*(i-1)), hold on
-    for ii = 1:aux.nbr_pws
-        plot(aux.t_evoked_force,force.analysis.mean(:,i,ii),'color',aux.colors(ii,:),'linewidth',2);
-    end
-    xlim([aux.t_evoked_force(1) aux.t_evoked_force(end)])
-    set(gca,'FontSize',16)
-end
-
-% add legends and axis labels
-for i = 1:force.nbr_forces
-    subplot(force.nbr_forces,2,1+2*(i-1))
-    ylabel(force.labels{i},'FontSize',16);
-    legend(squeeze(aux.color_h(i,1,:)),strread(num2str(force.meta.stim_pws),'%s'),'FontSize',16);
     if i == force.nbr_forces
-       xlabel('time (ms)','FontSize',16) 
+        xlabel('time (s)','FontSize',14)
     end
+    ylabel(force.labels{i},'FontSize',14)
+    set(gca,'FontSize',14),set(gca,'TickDir','out')
 end
+legend(axl,lgnd),%legend('boxoff')
+
+% % add legends and axis labels
+% for i = 1:force.nbr_forces
+%     subplot(force.nbr_forces,2,1+2*(i-1))
+%     ylabel(force.labels{i},'FontSize',16);
+%     legend(squeeze(aux.color_h(i,1,:)),strread(num2str(force.meta.stim_pws),'%s'),'FontSize',16);
+%     if i == force.nbr_forces
+%        xlabel('time (ms)','FontSize',16) 
+%     end
+% end
 
 %------------------------------------
 % 2. force vs. PW
-
-% PW axis, in us
-aux.pw_axis             = round(force.meta.stim_pws*1000); % round because sometimes linspace does weird things with the precision
 
 % plot the peak rectified force for each trial
 for i = 1:force.nbr_forces
     subplot(force.nbr_forces,2,2*i), hold on
     for ii = 1:aux.nbr_pws
-        plot(aux.pw_axis(ii),force.analysis.peak(:,i,ii),'marker','o','markersize',12,'color',aux.colors(ii,:));
+        plot(aux.pw_axis(ii),force.analysis.peak_rect(:,i,ii),'marker','o','markersize',12,'color',aux.colors(ii,:));
     end
     xlim([aux.pw_axis(1)-mean(diff(aux.pw_axis)), ...
         aux.pw_axis(end)+mean(diff(aux.pw_axis))]);
-    set(gca,'FontSize',16)
-end
-
-% and the mean force per PW
-for i = 1:force.nbr_forces
-    subplot(force.nbr_forces,2,2*i), hold on
-    plot(aux.pw_axis,force.analysis.mean_peak(i,:),'k','linewidth',2)
-    plot(aux.pw_axis,force.analysis.mean_peak(i,:) + ...
-        force.analysis.std_peak(i,:),'.-k','linewidth',1)
-    plot(aux.pw_axis,force.analysis.mean_peak(i,:) - ...
-        force.analysis.std_peak(i,:),'.-k','linewidth',1)
-end
-
-% add legends and axis labels
-for i = 1:force.nbr_forces
-    subplot(force.nbr_forces,2,2*i),
-    ylabel(force.labels{i},'FontSize',16);
+    % plot the mean +/- SD
+    plot(aux.pw_axis,force.analysis.mean_peak_rect(i,:),'k','linewidth',2)
+    plot(aux.pw_axis,force.analysis.mean_peak_rect(i,:) + ...
+        force.analysis.std_peak_rect(i,:),'.-','color',[.5 .5 .5],'linewidth',1)
+    plot(aux.pw_axis,force.analysis.mean_peak_rect(i,:) - ...
+        force.analysis.std_peak_rect(i,:),'.-k','color',[.5 .5 .5],'linewidth',1)
+    set(gca,'FontSize',14),set(gca,'TickDir','out')
+    ylabel([force.labels{i} ' rectif'],'FontSize',14)
     if i == force.nbr_forces
-       xlabel('pulse width (us)','FontSize',16) 
+        xlabel('pulse width (us)','FontSize',14)
     end
 end
+
+%------------------------------------
+% 2. Polar plot with force vs. PW
+
+% % plot the peak rectified force for each trial
+% for i = 1:force.nbr_forces
+%     subplot(force.nbr_forces,3,2*i), hold on
+%     for ii = 1:aux.nbr_pws
+%         plot(aux.pw_axis(ii),force.analysis.peak_rect(:,i,ii),'marker','o','markersize',12,'color',aux.colors(ii,:));
+%     end
+%     xlim([aux.pw_axis(1)-mean(diff(aux.pw_axis)), ...
+%         aux.pw_axis(end)+mean(diff(aux.pw_axis))]);
+%     % plot the mean +/- SD
+%     plot(aux.pw_axis,force.analysis.mean_peak_rect(i,:),'k','linewidth',2)
+%     plot(aux.pw_axis,force.analysis.mean_peak_rect(i,:) + ...
+%         force.analysis.std_peak_rect(i,:),'.-','color',[.5 .5 .5],'linewidth',1)
+%     plot(aux.pw_axis,force.analysis.mean_peak_rect(i,:) - ...
+%         force.analysis.std_peak_rect(i,:),'.-k','color',[.5 .5 .5],'linewidth',1)
+%     set(gca,'FontSize',14),set(gca,'TickDir','out')
+% end
