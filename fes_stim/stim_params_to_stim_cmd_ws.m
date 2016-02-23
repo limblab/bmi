a4392c34cec11ce11f6113ba89fcb67fe1d44b3c
juangallego <jotagalabe@gmail.com>
2016-02-23 16:33:14 -0600

    convert stim struct into wireless stimulator commands

--- fes_stim/stim_params_to_stim_cmd_ws.m
+++ fes_stim/stim_params_to_stim_cmd_ws.m
@@ -4,8 +4,6 @@
 %
 %   stim_cmd = stim_params_to_stim_string(stim_params)
 %
-
-
 %   The input argument is a stim_params structure, as described in
 %   stim_params_defaults.m
 %   
@@ -17,7 +15,7 @@
 %   'amp'       : amplitude of single phase's current (mA)
 %   'pol'       : Polarity. 1 - cathodic first, 0 - anodic first
 %
-%   Other than chan_list, which contains N electrode numbers, and stim_res,
+%   Other than elect_list, which contains N electrode numbers, and stim_res,
 %   which is a scalar, all the other fields may contain either a single
 %   value or a vector of N values. If only one value is provided, it will
 %   be used for all the stimulation channels listed in chan_list.
@@ -32,9 +30,15 @@
 %   stim_string = stim_params_to_stim_string(stim_params);
 %
 %   see also: stim_params_defaults.m, ezstim.m
+% 
+% -------------------------------------------------------------------------
+% NOTE !!!: the current version of the wireless stimulator firmware doesn't
+% support setting the polarity as an array so this has to be habdled in 
+%
 
 function stim_cmd = stim_params_to_stim_cmd_ws( stim_params )
 
+    
 % To ensure that the stimulation waveform will have the expect shape, we
 % have to allow for a TD > 50.
 if stim_params.delay < 50
@@ -42,12 +46,11 @@ if stim_params.delay < 50
 end
 
 % Single element struct array with param settings
-stim_cmd{1}     = struct('TL',      100, ...
-                         'Freq',    stim_params.freq, ...
-                         'CathAmp', 32768+stim_params.amp/32*32768, ...
-                         'AnodAmp', 32768-stim_params.amp/32*32768, ...
-                         'CathDur', stim_params.pw*1000, ...
-                         'AnodDur', stim_params.pw*1000, ...
-                         'TD',      stim_params.delay, ...           
-                         'PL',      stim_params.pol ...
-                         );
+stim_cmd{1}     = struct(   'TL',      stim_params.tl, ...
+                            'Freq',    stim_params.freq, ...
+                            'CathAmp', 32768+stim_params.amp*1000, ...
+                            'AnodAmp', 32768-stim_params.amp*1000 );
+
+stim_cmd{2}     = struct(   'CathDur', stim_params.pw*1000, ...
+                            'AnodDur', stim_params.pw*1000, ...
+                            'TD',      stim_params.delay );
