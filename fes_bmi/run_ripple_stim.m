d7184a14a51e7d8062379ec49c7c2a89861115ae
maripeza <mkharp@gmail.com>
2016-03-18 14:23:33 -0500

    make stim file for rats

--- fes_bmi/run_ripple_stim.m
+++ fes_bmi/run_ripple_stim.m
@@ -11,3 +11,29 @@ function varargout = run_ripple_stim(varargin)
 % 7. read the signal from 4; as each pair of high/low is sent, check if the
 % amplitude needs to be changed for the next pair. If so, send the updated
 % amp to the stimulator. 
+
+emg = struct; %read emgs into this struct formatted as: 'muscle'; [values over time]
+[cols, headers] = xlsread('fake_emg.xlsx'); %muscles (headers) and cols (each emg is in a column)
+muscles = strtrim(headers); %strip spaces from headers
+%plot(cols(:,2))
+hold on;
+cols = abs(cols); 
+plot(cols(:,2))
+%disp(muscles(2))
+
+for m=1:length(muscles)
+    [emg.(muscles{m})] = cols(:,m); %get field name from column header, put column in as vector
+end
+
+
+[b, a] = butter(9,.0055,'high');
+butterfilt = filtfilt(b, a, cols(:,2));
+plot(butterfilt); 
+
+hold off; 
+%**MUST start at 0, end at 0 regardless of what the fit says
+
+
+%okay, now send this signal that we just made
+%????
+
